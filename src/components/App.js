0e6e43151d0c0a799f52edc3ca3a326ba11fc3f2
Rishav Sharma <61363076+sethrishav@users.noreply.github.com>
2021-06-25 15:56:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,91 +2,37 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+     constructor(){
+         super();
+         this.state={
+             time: new Date()
+         };
+     }
+    
+     currentTime(){
+          this.setState({ time:new Date()});
+      }
+      componentDidMount(){
+          this.interval = setInterval(()=>
+              this.currentTime(),1000);
+      }
+      componentWillUnmount(){
+          clearInterval(this.interval);
+      }
+    
+    render() {
 
- constructor() {
-
- super()
-
- this.state = {
-
- time: new Date().toLocaleTimeString()
-
- }
-
- 
-
-this.dateFunction = this.dateFunction.bind(this)
-
- }
-
- 
-
-dateFunction() {
-
- let date = new Date()
-
- let updateHour = date.getHours()
-
- let updateMinute = date.getMinutes()
-
- let updateSecond = date.getSeconds()
-
- 
-
-let updateAMPM = updateHour < 13 ? 'AM' : 'PM'
-
- if(updateMinute < 10) updateMinute = `0${updateMinute}`
-
- if(updateSecond < 10) updateSecond = `0${updateSecond}`
-
- let updateTime = `${updateHour}:${updateMinute}:${updateSecond} ${updateAMPM}`
-
- 
-
-this.setState({time: updateTime})
-
- }
-
- 
-
-componentDidMount() {
-
- this.loadInterval = setInterval(this.dateFunction, 1000)
-
- }
-
- 
-
-componentWillUnmount() {
-
- clearInterval(this.loadInterval)
-
- }
-
- 
-
-render() {
-
- 
-
-return(
-
- <>
-
- <div className="Clock">
-
- <h3 id="time">{this.state.time}</h3>
-
- </div>
-
- </>
-
- )
-
- }
-
+        return(
+            <>
+                   
+                <div className="Clock">
+               <h3 id="time">{this.state.time.toLocaleTimeString()}</h3>
+           </div>
+            
+            </>
+        )
+    }
 }
 
- 
 
 export default App;
